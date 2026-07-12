fa6c8692026b24e340d4d8c045c611d1129965a6
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2026-07-12 15:43:54 +0900

    use consistent value for num_sub_vectors

--- python/python/benchmarks/test_index.py
+++ python/python/benchmarks/test_index.py
@@ -48,7 +48,7 @@ def test_create_ivf_pq(test_dataset, benchmark):
         index_type="IVF_PQ",
         metric_type="L2",
         num_partitions=8,
-        num_sub_vectors=N_DIMS / 16,
+        num_sub_vectors=2,
         num_bits=8,
         replace=True,
     )
